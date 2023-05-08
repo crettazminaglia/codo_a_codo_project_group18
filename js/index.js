3c110f29ef5eeb3ec7700ed34c9bbef71d84b312
longoD3V <longom_19@hotmail.com>
2023-05-08 14:56:06 -0300

    1

--- js/index.js
+++ js/index.js
@@ -6,3 +6,14 @@ var_precio = 50;
 var_oferta = var_precio * 0.5;
 
 console.log(var_oferta);
+
+class Auto{
+    constructor(marca,color,año,modelo){
+        this.marca = marca;
+        this.color = color;
+        this.año = año;
+        this.modelo = modelo;
+    }
+}
+let a1 = new Auto("Nissan","Gris",2019,"March");
+console.log(a1);
